bc3e8da95cfe29bf32f506f55ba2022bd3f6bed3
Robert Patrick Chamberlain <robertpatrick.chamb@learnet.se>
2026-06-10 15:02:02 +0200

    Test

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,9 @@
 import { JSX } from "react";
 
-export default function About():JSX.Element{
+export default function About() {
 
-return(<div className="max-w-6xl mx-auto space-y-4">
+return(
+<div className="max-w-6xl mx-auto space-y-4">
   <h1 className="text-2xl font-medium text-amber-600 ">About</h1>
   <p>
         Welcome to our movie database website! We are a team of passionate movie
