feeaaba86cb28631d3958c366f69ca0deec28538
Robert Patrick Chamberlain <robertpatrick.chamb@learnet.se>
2026-06-10 15:15:29 +0200

    Test

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,10 +3,7 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <div className="text-center mt-20">
-      <h1 className="text-3xl font-bold">404 - Page Not Found</h1>
-      <Link href="/" className="text-amber-600 mt-4 inline-block">
-        Go back home
-      </Link>
+      oops
     </div>
   )
 }
